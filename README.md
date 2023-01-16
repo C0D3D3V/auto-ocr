4d5767d69b34782134644253e137dcc2e2df3f69
c0d3d3v <c0d3d3v@mag-keinen-spam.de>
2023-01-16 21:56:59 +0100

    copy from https://github.com/cmccambridge/ocrmypdf-auto

---
+++ README.md
@@ -0,0 +1,22 @@
+<div align="center">
+    <br>
+    <h2>Auto OCR</h2>
+    <small>Tool to automatically OCR PDFs and copy them to a folder</small> 
+    <br>
+</div>
+
+---
+
+## 🚀 Setup
+
+1. Install [Python](https://www.python.org/) >=3.7
+2. Run `pip install .` inside the repository.
+3. Run `auto-ocr -h` to see all options and read the source code to understand the usage
+
+---
+
+
+
+## ⚖️ License
+
+This project is licensed under the MIT License - see the [LICENSE](LICENSE) file for details
---
+++ setup.py
@@ -0,0 +1,47 @@
+from os import path
+from setuptools import setup, find_packages
+
+
+# Get the version from atom_dl/version.py without importing the package
+exec(compile(open('atom_dl/version.py').read(), 'atom_dl/version.py', 'exec'))
+
+
+def readme():
+    this_directory = path.abspath(path.dirname(__file__))
+    with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+        return f.read()
+
+
+setup(
+    name='auto-ocr',
+    version=__version__,
+    description='Tool to automatically OCR PDFs and copy them to a folder',
+    long_description=readme(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/c0d3d3v/auto-ocr',
+    author='Colin McCambridge, c0d3d3v',
+    author_email='c0d3d3v@mag-keinen-spam.de',
+    license='MIT',
+    packages=find_packages(),
+    include_package_data=True,
+    entry_points={
+        'console_scripts': [
+            'auto-ocr = auto_ocr.main:main',
+        ],
+    },
+    python_requires='>=3.7',
+    install_requires=[
+'ocrmypdf>=14.0.2',
+'watchdog~=0.10.3'
+    ],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Console',
+        'Intended Audience :: End Users/Desktop',
+        'License :: OSI Approved :: MIT License (MIT)',
+        'Programming Language :: Python :: 3 :: Only',
+        'Topic :: Education',
+        'Topic :: Utilities',
+    ],
+    zip_safe=False,
+)
