22439f074dd063d60b50c96326876878b94dbe3c
c0d3d3v <c0d3d3v@mag-keinen-spam.de>
2023-01-17 01:04:59 +0100

    Remember done jobs

--- auto_ocr/jobs_processor.py
+++ auto_ocr/jobs_processor.py
@@ -1,4 +1,5 @@
-from typing import Dict
+import os
+from typing import Dict, List
 
 from auto_ocr.utils import PathTools as PT, Log, append_list_to_json, load_list_from_json
 
@@ -14,18 +15,41 @@ class JobsProcessor:
         self.path_of_done_pdfs_json = PT.get_path_of_done_pdfs_json()
         self.done_pdfs = load_list_from_json(self.path_of_done_pdfs_json)
 
+    def get_done_pdfs_for_this_job(self, job_name) -> List[str]:
+        that_job_done_pdfs = []
+        for done_pdf in self.done_pdfs:
+            done_pdf_job_name = done_pdf.get('job_name', None)
+            if done_pdf_job_name is None or job_name != done_pdf_job_name:
+                continue
+            done_pdf_filename = done_pdf.get('filename', None)
+            if done_pdf_filename is not None:
+                that_job_done_pdfs.append(done_pdf_filename)
+
+        return that_job_done_pdfs
+
     def process_job(self, job: Dict):
         source_dir = job.get('source_dir', None)
         destination_dir = job.get('destination_dir', None)
-        if source_dir is None or destination_dir is None:
+        job_name = job.get('name', None)
+        if source_dir is None or destination_dir is None or job_name is None:
             Log.error('source_dir or destination_dir not set for job')
             return
 
         source_dir = PT.get_abs_path(source_dir)
+        PT.make_dirs(source_dir)
         destination_dir = PT.get_abs_path(destination_dir)
+        PT.make_dirs(destination_dir)
 
-        finished_pdfs = []
-        append_list_to_json(self.path_of_done_pdfs_json, finished_pdfs)
+        that_job_done_pdfs = self.get_done_pdfs_for_this_job(job_name)
+        now_finished_pdfs = []
+
+        pdf_names = os.listdir(source_dir)
+        for pdf_name in pdf_names:
+            if pdf_name not in that_job_done_pdfs:
+                print(pdf_name)
+                now_finished_pdfs.append({'filename': pdf_name, 'job_name': job_name})
+
+        append_list_to_json(self.path_of_done_pdfs_json, now_finished_pdfs)
 
     def process(self):
         for job in self.job_definitions:
